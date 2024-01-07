97e409ba406f039c1b4dbb996d751555cbb8bb37
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-07 18:08:28 +0000

    with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.ts"],
+
+  theme: {
+    extend: {},
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
+
